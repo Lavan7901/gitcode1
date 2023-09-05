38759624e4d904da909499b384574ec3a48adabc
Lavanya <lavanyaj0725@gmail.com>
2023-09-05 13:45:48 +0530

    files

---
+++ Components/About.jsx
@@ -0,0 +1,20 @@
+import React from'react';
+import aboutimage from '../Assets/images(8).png';
+
+function About() {
+    return(
+        <div id='About'>
+            <div classNmae='about-image'>
+                <imd src={ aboutimage} alt=''/>
+            </div>
+            <div classNmae='about-text'>
+                <h1>jhfefn</h1>
+                <p>ewhrewjfewhrwuiruuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuur3uy2i4h3yugr32hyugru3bfi3uvg3urb3uy</p>
+                <button> Learn More</button>           
+                 </div>
+        </div>
+    )
+}
+
+
+export default About;
